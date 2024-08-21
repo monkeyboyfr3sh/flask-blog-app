4afbe5136c32042ea987012a61ff8d2e16c25d6e
david <monkeyboyfresh_bro@yahoo.com>
2024-08-20 22:54:55 -0500

    making repo

---
+++ flaskblog/main/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template, request, Blueprint
+
+from flaskblog.models import Post
+from sqlalchemy.exc import OperationalError
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    page = request.args.get("page", 1, type=int)
+    try:
+        posts = Post.query.order_by(Post.date_posted.desc()).paginate(
+            page=page, per_page=5
+        )
+    except OperationalError:
+        posts = None
+    return render_template("home.html", posts=posts)
+
+
+@main.route("/about")
+def about():
+    return render_template("about.html", title="About")
