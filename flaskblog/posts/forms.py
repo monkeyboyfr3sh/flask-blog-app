2e749e9ad38ec6b6085976da807b9ab5f688922f
david <monkeyboyfresh_bro@yahoo.com>
2024-09-19 13:30:33 +0000

    fix

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -6,10 +6,10 @@ from flask_wtf.file import FileAllowed
 class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
-    image = FileField("Attach Image", validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    image = FileField("Attach Image", validators=[FileAllowed(['jpg', 'png', 'jpeg','gif'])])
     submit = SubmitField("Post")
 
 class CommentForm(FlaskForm):
     content = TextAreaField('Comment', validators=[DataRequired()])
-    image = FileField("Attach Image", validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    image = FileField("Attach Image", validators=[FileAllowed(['jpg', 'png', 'jpeg','gif'])])
     submit = SubmitField('Post Comment')
